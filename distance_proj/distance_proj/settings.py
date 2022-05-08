0af66db63f894dc2ba42b59fbce540e31420e2c9
mrserjo <mrserjo@mail.ru>
2022-05-08 18:46:39 +0300

    hw

--- distance_proj/distance_proj/settings.py
+++ distance_proj/distance_proj/settings.py
@@ -114,6 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- distance_proj/measurements/views.py
+++ distance_proj/measurements/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Measurement
 from .forms import MeasurementModelForm
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+from .utils import get_geo
 
 
 def calculate_distance_view(request):
     obj = get_object_or_404(Measurement, id=1)
     form = MeasurementModelForm(request.POST or None)
+    geolocator = Nominatim(user_agent='measurements')
+
+    ip = '72.14.207.99'
+    country, city, lat, lon = get_geo(ip)
+    # Дальнейшие принты на случай, если потерялся, что и как выдает
+    # print('location country', country)
+    # print('location city', city)
+    # print('location lat, lon', lat, lon)
+
+    location = geolocator.geocode(city)
+    # print('###', location)
+
+    l_lat = lat
+    l_lon = lon
+    pointA = (l_lat, l_lon)
 
     if form.is_valid():
         instance = form.save(commit=False)
-        instance.destination = form.cleaned_data.get('destination')
-        instance.location = 'San Francisco'
-        instance.distance = 5000.00
+        destination_ = form.cleaned_data.get('destination')
+        destination = geolocator.geocode(destination_)
+        # print(destination)
+        d_lat = destination.latitude
+        d_lon = destination.longitude
+
+        pointB = (d_lat, d_lon)
+
+        distance = round(geodesic(pointA, pointB).km, 2)
+
+        instance.location = location
+        instance.distance = distance
         instance.save()
 
     context = {
