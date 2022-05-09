2776f4c7e7f6e826505b765fb320bfea2a05db05
mrserjo <mrserjo@mail.ru>
2022-05-09 08:18:24 +0300

    map drawing added

--- distance_proj/measurements/views.py
+++ distance_proj/measurements/views.py
@@ -3,7 +3,8 @@ from .models import Measurement
 from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
-from .utils import get_geo
+from .utils import get_geo, get_center_coordinates
+import folium
 
 
 def calculate_distance_view(request):
@@ -13,37 +14,57 @@ def calculate_distance_view(request):
 
     ip = '72.14.207.99'
     country, city, lat, lon = get_geo(ip)
-    # Дальнейшие принты на случай, если потерялся, что и как выдает
-    # print('location country', country)
-    # print('location city', city)
-    # print('location lat, lon', lat, lon)
 
     location = geolocator.geocode(city)
-    # print('###', location)
 
+    # координаты нынешнего места расположения
     l_lat = lat
     l_lon = lon
     pointA = (l_lat, l_lon)
+
+    # Изначальная отрисовка карты
+    m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon))
+
+    # маркер местоположения
+    folium.Marker([l_lat, l_lon], tooltip='click here for more', popup=city['city'],
+                  icon=folium.Icon(color='purple')).add_to(m)
 
     if form.is_valid():
         instance = form.save(commit=False)
         destination_ = form.cleaned_data.get('destination')
         destination = geolocator.geocode(destination_)
-        # print(destination)
+
+        # координаты точки назначения
         d_lat = destination.latitude
         d_lon = destination.longitude
 
         pointB = (d_lat, d_lon)
 
+        # расчет расстояния
         distance = round(geodesic(pointA, pointB).km, 2)
+
+        # Модификация отрисовки карты
+        m = folium.Map(width=800, height=500,
+                       location=get_center_coordinates(l_lat, l_lon, d_lat, d_lon))
+
+        # маркер местоположения
+        folium.Marker([l_lat, l_lon], tooltip='click here for more', popup=city['city'],
+                      icon=folium.Icon(color='purple')).add_to(m)
+        # маркер точки назначения
+        folium.Marker([d_lat, d_lon], tooltip='click here for more', popup=destination,
+                      icon=folium.Icon(color='red', icon='cloud')).add_to(m)
 
         instance.location = location
         instance.distance = distance
         instance.save()
 
+
+    m = m._repr_html_()
+
     context = {
         'distance': obj,
-        'form': form
+        'form': form,
+        'map': m,
     }
 
     return render(request, 'measurements/main.html', context)
